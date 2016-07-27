dd6ae9b36d90d9fcf7e52fbc5a79f74b752f6e02
ericward <eric.ward@gmail.com>
2016-07-27 12:32:00 -0500

    Updated filenames in TRACC.r

--- TRACC.R
+++ TRACC.R
@@ -2,6 +2,6 @@ FLAG<-as.numeric(readline('Clean (1) or Convert (2)?'))
 while(FLAG!=1&FLAG!=2|is.na(FLAG)){
   FLAG<-as.numeric(readline('Bad Input. Enter 1 or 2.'))
 } 
-if(FLAG==1) source('CleanSapfluxData.r')
-if(FLAG==2) source('BaselineSapfluxData.r')
+if(FLAG==1) source('Clean.r')
+if(FLAG==2) source('Convert.r')
 
